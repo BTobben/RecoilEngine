aab6aa9d7d514445b52ad0133aa755c943c31c8e
Bryan Tobben <46655663+BTobben@users.noreply.github.com>
2026-07-19 21:51:25 +0200

    Keep runtime config outside BAR content

--- installer/Mac/bar-gl41-launcher.sh
+++ installer/Mac/bar-gl41-launcher.sh
@@ -7,9 +7,18 @@ RESOURCES="$CONTENTS/Resources"
 RUNTIME="$RESOURCES/runtime"
 BAR="$RESOURCES/BAR-content"
 WRITE_DIR="${BAR_WRITE_DIR:-$HOME/Library/Application Support/Beyond All Reason GL41}"
-CONFIG="${BAR_CONFIG:-$BAR/common/configs/macos-gl41.cfg}"
 
 mkdir -p "$WRITE_DIR"
+
+if [ -n "${BAR_CONFIG:-}" ]; then
+	CONFIG="$BAR_CONFIG"
+else
+	CONFIG="$WRITE_DIR/springsettings.cfg"
+	if [ ! -f "$CONFIG" ]; then
+		cp "$BAR/common/configs/macos-gl41.cfg" "$CONFIG"
+	fi
+fi
+
 export SPRING_DATADIR="$RUNTIME:$BAR"
 
 exec "$RUNTIME/spring" \
